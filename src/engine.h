d1f07791e575a577120714782fb1e7fe465b3a83
Xannicc <118775902+Xannicc@users.noreply.github.com>
2025-12-01 15:49:20 +1100

    sync

---
+++ src/engine.h
@@ -0,0 +1,16 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "utils.h"
+#include "constants.h"
+
+typedef enum {
+    PAWN = 1,
+    KNIGHT = 3,
+    BISHOP = 3,
+    ROOK = 5,
+    QUEEN = 9,
+} piece_values;
+
+#endif
+
